3a37212e9fed0001e77b13644c8b851b05f58c83
lgwoo <60616488+lgwoo@users.noreply.github.com>
2025-02-14 13:19:56 +0000

    go

--- pycoding_ago/S1_2667_단지번호붙이기.py
+++ pycoding_ago/S1_2667_단지번호붙이기.py
@@ -21,13 +21,32 @@ for i in range(n):
 # 필요변수 선연
 move = [(0,1),(1,0),(-1,0),(0,-1)]
 
+
 def bfs(start):
-    que = deque(start)
+    que = deque()
+    que.append(start)
+    matrix[start[0]][start[1]] = 0
+    counter = 0
     while que:
-        x,y,p = que.popleft()
+        xy = que.popleft()
+        x = xy[0]
+        y = xy[1]
+        counter +=1
         for i in range(4):
             dx = x+move[i][0]
             dy = y+move[i][1]
-            if 0<=dx<n-1 and 0<=dy<n-1:
+            if 0<=dx<n and 0<=dy<n:
                 if matrix[dx][dy] == 1:
-                    
+                    que.append((dx,dy))
+                    matrix[dx][dy] = 0 
+    return counter
+ans = []
+for i in range(n):
+    for k in range(n):
+        if matrix[i][k] == 1:
+            ans.append(bfs((i,k)))
+print(len(ans))
+ans.sort()
+for i in ans:
+    print(i)
+        
