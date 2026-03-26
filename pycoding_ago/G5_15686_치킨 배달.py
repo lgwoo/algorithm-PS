093a90ecb28d8084e63e4233b014201cc12f0dbd
lgwoo <ggg0811@daum.net>
2026-03-26 14:21:34 +0900

    G5치킨배달(완)

--- pycoding_ago/G5_15686_치킨 배달.py
+++ pycoding_ago/G5_15686_치킨 배달.py
@@ -1,24 +1,35 @@
 import sys
 input = sys.stdin.readline
+
 n,m = map(int, input().split())
 city = [list(map(int, input().split())) for _ in range(n)]
 
 house = []
 chicken = []
+combination = []
 for i in range(n):
     for j in range(n):
         if city[i][j]==1:
             house.append((i,j))
         elif city[i][j]==2:
             chicken.append((i,j))
+finalanswer = float('inf')
 
 #gpt가 짜준 조합 구하는 코드
 def comb(idx, path):
+    global finalanswer
     if len(path) == m:
-        print(path)
-        return
+            sumdist = 0
+            for housex,housey in house: # 여러 집들중 하나의 집의 최솟값 구하기
+                dist = float('inf')
+                for combix,combiy in path:
+                    dist = min(dist,abs(housex-combix)+abs(housey-combiy))
+                sumdist += dist
+            finalanswer = min(finalanswer, sumdist)
+            return
 
     for i in range(idx, len(chicken)):
         comb(i + 1, path + [chicken[i]])
 
 comb(0, [])
+print(finalanswer)
