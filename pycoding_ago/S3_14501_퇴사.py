94845dc053bc241dd5de9bbf5e2b703243f1287b
lgwoo <ggg0811@daum.net>
2026-04-04 19:38:16 +0900

    .

--- pycoding_ago/S3_14501_퇴사.py
+++ pycoding_ago/S3_14501_퇴사.py
@@ -7,7 +7,7 @@ plist = [0]*n
 dp = [0]*n
 for i in range(n):
     tlist[i],plist[i] = map(int,input().split())
-    if tlist[i]+i =>n:
+    if tlist[i]+i >=n:
         continue
     dp[i+tlist[i]] = max(dp[i]+plist[i],dp[i+tlist[i]])
        
